10a4459d184ba3cdeabcc696d17c8d75ad2e2c8f
Antoine HENRIO <antoine.henrio@doqboard.com>
2020-07-22 11:54:40 +0200

    avancée front

---
+++ client/scriptFooter.js
@@ -0,0 +1,7 @@
+function clickBtn1()
+{
+  var p = document.getElementById("btn1");
+  // NOTE: showAlert(); ou showAlert(param); NE fonctionne PAS ici.
+  // Il faut fournir une valeur de type function (nom de fonction déclaré ailleurs ou declaration en ligne de fonction).
+  p.onclick = showAlert;
+};
