fa40c7e64abfc26dbcbcbab1e2e157e576e32050
TheeAntoine <aboquier@outlook.fr>
2020-07-23 10:46:59 +0200

    Plat final fini

---
+++ client/scriptFooter.js
@@ -0,0 +1,38 @@
+function clickBtn1()
+{
+  var p = document.getElementById("btn1");
+  var p2 = document.getElementById("btn2");
+  var p3 = document.getElementById("btn3");
+  p.className = 'imgFooterclicked';
+  p2.className = 'imgFooter'
+  p3.className = 'imgFooter'
+  p.setAttribute("src", "./images/Home blanc.png");
+  p2.setAttribute("src", "./images/Verre bleu.png");
+  p3.setAttribute("src", "./images/Profil bleu.png");
+};
+
+function clickBtn2()
+{
+    var p = document.getElementById("btn1");
+    var p2 = document.getElementById("btn2");
+    var p3 = document.getElementById("btn3");
+    p.className = 'imgFooter';
+    p2.className = 'imgFooterclicked'
+    p3.className = 'imgFooter'
+    p.setAttribute("src", "./images/Home bleu.png");
+    p2.setAttribute("src", "./images/Verre blanc.png");
+    p3.setAttribute("src", "./images/Profil bleu.png");
+};
+
+function clickBtn3()
+{
+    var p = document.getElementById("btn1");
+    var p2 = document.getElementById("btn2");
+    var p3 = document.getElementById("btn3");
+    p.className = 'imgFooter';
+    p2.className = 'imgFooter'
+    p3.className = 'imgFooterclicked'
+    p.setAttribute("src", "./images/Home bleu.png");
+    p2.setAttribute("src", "./images/Verre bleu.png");
+    p3.setAttribute("src", "./images/Profil blanc.png");
+};
