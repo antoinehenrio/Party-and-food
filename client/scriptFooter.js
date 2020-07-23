b5b36f0656dbdf9001343c79161e48d106ca2618
nim27730 <benjamin.sy@viacesi.fr>
2020-07-23 14:15:32 +0200

    detailsSoiree

--- client/scriptFooter.js
+++ client/scriptFooter.js
@@ -1,7 +1,38 @@
 function clickBtn1()
 {
     var p = document.getElementById("btn1");
-    // NOTE: showAlert(); ou showAlert(param); NE fonctionne PAS ici.
-    // Il faut fournir une valeur de type function (nom de fonction déclaré ailleurs ou declaration en ligne de fonction).
-    p.onclick = showAlert;
+    var p2 = document.getElementById("btn2");
+    var p3 = document.getElementById("btn3");
+    p.className = 'imgFooterclicked';
+    p2.className = 'imgFooter'
+    p3.className = 'imgFooter'
+    p.setAttribute("src", "client/images/Home blanc.png");
+    p2.setAttribute("src", "client/images/Verre bleu.png");
+    p3.setAttribute("src", "client/images/Profil bleu.png");
+};
+
+function clickBtn2()
+{
+    var p = document.getElementById("btn1");
+    var p2 = document.getElementById("btn2");
+    var p3 = document.getElementById("btn3");
+    p.className = 'imgFooter';
+    p2.className = 'imgFooterclicked'
+    p3.className = 'imgFooter'
+    p.setAttribute("src", "client/images/Home bleu.png");
+    p2.setAttribute("src", "client/images/Verre blanc.png");
+    p3.setAttribute("src", "client/images/Profil bleu.png");
+};
+
+function clickBtn3()
+{
+    var p = document.getElementById("btn1");
+    var p2 = document.getElementById("btn2");
+    var p3 = document.getElementById("btn3");
+    p.className = 'imgFooter';
+    p2.className = 'imgFooter'
+    p3.className = 'imgFooterclicked'
+    p.setAttribute("src", "client/images/Home bleu.png");
+    p2.setAttribute("src", "client/images/Verre bleu.png");
+    p3.setAttribute("src", "client/images/Profil blanc.png");
 };
---
+++ client/scriptHidden.js
@@ -0,0 +1,50 @@
+function clickCheck1(){
+    var p = document.getElementById("check1");
+    if (p.className == "checkIcone") {
+        p.className = "checkIconeHidden";
+    }
+    else {
+        console.log("oui");
+        p.className = "checkIcone";
+    }
+}
+
+function clickCheck2(){
+    var p = document.getElementById("check2");
+    if (p.className == "checkIcone") {
+        p.className = "checkIconeHidden";
+    }
+    else {
+        p.className = "checkIcone";
+    }
+}
+
+function clickModal1(){
+    var p = document.getElementById("modal1");
+    p.className = "modal";
+}
+
+function clickModal2(){
+    var p = document.getElementById("modal2");
+    p.className = "modal";
+}
+
+function clickModal3(){
+    var p = document.getElementById("modal3");
+    p.className = "modal";
+}
+
+function clickCross1(){
+    var p = document.getElementById("modal1");
+    p.className = "modalHidden";
+}
+
+function clickCross2(){
+    var p = document.getElementById("modal2");
+    p.className = "modalHidden";
+}
+
+function clickCross3(){
+    var p = document.getElementById("modal3");
+    p.className = "modalHidden";
+}
